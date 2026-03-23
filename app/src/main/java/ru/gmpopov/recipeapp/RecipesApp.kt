acf05e5c22c286eb1fafd4d573602c18c3e74f55
gpopov <pgriga@mail.ru>
2026-03-23 22:04:17 +0500

    RCA-10: create core\ui and favoritesScreen

--- app/src/main/java/ru/gmpopov/recipeapp/RecipesApp.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/RecipesApp.kt
@@ -1,18 +1,13 @@
 package ru.gmpopov.recipeapp
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ru.gmpopov.recipeapp.core.ui.navigation.BottomNavigation
 import ru.gmpopov.recipeapp.ui.categories.CategoriesScreen
 import ru.gmpopov.recipeapp.ui.favorites.FavoritesScreen
