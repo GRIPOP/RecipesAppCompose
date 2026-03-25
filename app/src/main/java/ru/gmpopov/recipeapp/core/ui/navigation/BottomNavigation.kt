5643fa4335b28727a23e3f483c7b38e3db8d18c5
gpopov <pgriga@mail.ru>
2026-03-25 19:51:06 +0500

    RCA-08: bug fix

--- app/src/main/java/ru/gmpopov/recipeapp/core/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/core/ui/navigation/BottomNavigation.kt
@@ -1,8 +1,9 @@
-package ru.gmpopov.recipeapp.cori.ui.navigation
+package ru.gmpopov.recipeapp.core.ui.navigation
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,7 +13,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.gmpopov.recipeapp.ui.theme.Dimens
@@ -25,28 +25,33 @@ fun BottomNavigation(
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .navigationBarsPadding()
+            .padding(horizontal = Dimens.PaddingMain)
+
     ) {
         Button(
-            onClick = {},
+            onClick = { onCategoriesClick() },
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.tertiary,
-                contentColor = MaterialTheme.colorScheme.onTertiary
+                contentColor = MaterialTheme.colorScheme.onTertiary,
             ),
+            shape = RoundedCornerShape(8.dp),
             modifier = Modifier
-                .padding(Dimens.PaddingMain)
                 .weight(1f)
-        ){
+        ) {
             Text("Категории")
         }
 
+        Spacer(modifier = Modifier.width(Dimens.PaddingMain))
+
         Button(
-            onClick = {},
+            onClick = { onFavoriteClick() },
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.error,
                 contentColor = MaterialTheme.colorScheme.onError
             ),
+            shape = RoundedCornerShape(8.dp),
             modifier = Modifier
-                .padding(Dimens.PaddingMain)
                 .weight(1f)
         ) {
             Text("Избранное")
