c1f8c51ab7b5ebbc81d4374b5f0c525b5b973804
gpopov <pgriga@mail.ru>
2026-03-28 13:33:00 +0500

    RCA-14: add CategoryItem

--- app/src/main/java/ru/gmpopov/recipeapp/ui/categories/CategoryItem.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/ui/categories/CategoryItem.kt
@@ -13,12 +13,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import ru.gmpopov.recipeapp.R
 import ru.gmpopov.recipeapp.ui.categories.model.CategoryUiModel
 import ru.gmpopov.recipeapp.ui.theme.Dimens
-
 
 @Composable
 fun CategoryItem(
