f0b2643e726b6e1d7d654327402f8e675c222d84
gpopov <pgriga@mail.ru>
2026-05-10 14:22:40 +0500

    RCA-26: create CategoriesUiState and CategoriesViewModel

---
+++ app/src/main/java/ru/gmpopov/recipeapp/features/categories/presentation/CategoriesViewModel.kt
@@ -0,0 +1,46 @@
+package ru.gmpopov.recipeapp.features.categories.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ru.gmpopov.recipeapp.data.repository.RecipesRepositoryStub
+import ru.gmpopov.recipeapp.features.categories.presentation.model.CategoriesUiState
+import ru.gmpopov.recipeapp.features.categories.presentation.model.toUiModel
+
+class CategoriesViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(CategoriesUiState())
+    val uiState: StateFlow<CategoriesUiState> = _uiState.asStateFlow()
+
+    init {
+        _uiState.update { currentState ->
+            currentState.copy(isLoading = true)
+        }
+
+        viewModelScope.launch {
+            try {
+                val categories =
+                    RecipesRepositoryStub.getCategories()
+                        .map { categoryDto -> categoryDto.toUiModel() }
+
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        categories = categories,
+                        isLoading = false
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        isLoading = false,
+                        error = "ошибка загрузки данных"
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ru/gmpopov/recipeapp/features/categories/presentation/model/CategoriesUiState.kt
@@ -0,0 +1,7 @@
+package ru.gmpopov.recipeapp.features.categories.presentation.model
+
+data class CategoriesUiState(
+    val categories: List<CategoryUiModel> = emptyList(),
+    var isLoading: Boolean = false,
+    val error: String? = null
+)
