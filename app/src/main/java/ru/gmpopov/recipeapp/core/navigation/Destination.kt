d3d75c169b79b07c5e5ce55ac1eb4625d1659f3b
gpopov <pgriga@mail.ru>
2026-05-18 20:02:08 +0500

    RCA-27: change Uri.encode to URLEncoder.encode

--- app/src/main/java/ru/gmpopov/recipeapp/core/navigation/Destination.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/core/navigation/Destination.kt
@@ -2,12 +2,17 @@ package ru.gmpopov.recipeapp.core.navigation
 
 import android.net.Uri
 import ru.gmpopov.recipeapp.core.utils.DEEP_LINK_BASE_URL
+import java.net.URLEncoder
+
 sealed class Destination(val route: String) {
     object Categories : Destination("categories")
     object Favorites : Destination("favorites")
     object Recipes : Destination("recipes/{categoryId}/{categoryTitle}/{categoryImageUrl}") {
         fun createRoute(categoryId: Int, categoryTitle: String, categoryImageUrl: String) =
-            "recipes/$categoryId/${Uri.encode(categoryTitle)}/${Uri.encode(categoryImageUrl)}"
+            "recipes/" +
+                    "$categoryId/" +
+                    "${URLEncoder.encode(categoryTitle, "UTF-8")}/" +
+                    "${URLEncoder.encode(categoryImageUrl, "UTF-8")}"
     }
 
     object RecipeItem : Destination("recipe/{recipeId}") {
--- app/src/main/java/ru/gmpopov/recipeapp/features/recipes/presentation/RecipesViewModel.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/features/recipes/presentation/RecipesViewModel.kt
@@ -3,19 +3,18 @@ package ru.gmpopov.recipeapp.features.recipes.presentation
 import android.net.Uri
 import androidx.lifecycle.SavedStateHandle
 import ru.gmpopov.recipeapp.data.repository.RecipesRepositoryStub
-import ru.gmpopov.recipeapp.features.recipes.presentation.model.RecipeUiModel
 import ru.gmpopov.recipeapp.features.recipes.presentation.model.RecipesUiState
 import ru.gmpopov.recipeapp.features.recipes.presentation.model.toUiModel
 
 class RecipesViewModel(
     private val savedStateHandle: SavedStateHandle
 ) {
-    val categoryId: Int = savedStateHandle.get("categoryId")
+    val categoryId: Int = savedStateHandle["categoryId"]
         ?: throw IllegalArgumentException("Категория не найдена")
 
     val uiState = RecipesUiState(
-        categoryTitle = Uri.decode(savedStateHandle.get("categoryTitle") ?: ""),
-        categoryImageUrl = Uri.decode(savedStateHandle.get("categoryImageUrl") ?: ""),
+        categoryTitle = Uri.decode(savedStateHandle["categoryTitle"] ?: ""),
+        categoryImageUrl = Uri.decode(savedStateHandle["categoryImageUrl"] ?: ""),
         recipes = RecipesRepositoryStub.getRecipesByCategoryId(categoryId).map { it.toUiModel() },
     )
 }
