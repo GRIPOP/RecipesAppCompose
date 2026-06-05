2d7a5b4fe198f35708b586f3f094cdf4bc28c970
gpopov <pgriga@mail.ru>
2026-06-05 20:17:37 +0500

    RCA-37: change PREFS_FILE_NAME to IMAGES_BASE_URL in maaping DRO to UI

--- app/src/main/java/ru/gmpopov/recipeapp/core/utils/Constants.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/core/utils/Constants.kt
@@ -1,6 +1,5 @@
 package ru.gmpopov.recipeapp.core.utils
 
-const val ASSETS_URI_PREFIX = "file:///android_asset/"
 
 const val DEEP_LINK_SCHEME = "recipeapp"
 const val DEEP_LINK_BASE_URL = "https://recipes.androidsprint.ru"
