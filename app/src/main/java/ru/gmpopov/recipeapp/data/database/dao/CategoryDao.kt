d38eaff35066070df4965e52d975e9c78d068cd7
gpopov <pgriga@mail.ru>
2026-06-12 10:09:40 +0500

    RCA-40: add fun insertOrUpdateCategory in CategoryDao

--- app/src/main/java/ru/gmpopov/recipeapp/data/database/dao/CategoryDao.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/data/database/dao/CategoryDao.kt
@@ -1,12 +1,18 @@
 package ru.gmpopov.recipeapp.data.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import ru.gmpopov.recipeapp.data.database.entity.CategoryEntity
+import ru.gmpopov.recipeapp.data.database.entity.RecipeEntity
 
 @Dao
 interface CategoryDao {
     @Query("SELECT * FROM categories ORDER BY name")
     fun getAllCategories(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrUpdateCategory(categories: List<CategoryEntity>)
 }
