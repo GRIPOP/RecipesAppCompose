15d27798dce9e5a6bdca81b8649a8e65da14f9f9
gpopov <pgriga@mail.ru>
2026-06-14 12:44:19 +0500

    RCA-39: add check empty string in converter and add column info for categoryId in RecipeEntity

--- app/src/main/java/ru/gmpopov/recipeapp/data/database/converter/Converters.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/data/database/converter/Converters.kt
@@ -6,12 +6,15 @@ import androidx.room.TypeConverter
 class Converters {
     @TypeConverter
     fun fromString(data: String): List<String> {
-        return data.split("|||")
+        return if (data.isEmpty()) {
+            emptyList()
+        } else {
+            data.split("|||")
+        }
     }
 
     @TypeConverter
-    fun fromList(data: List<String>): String
-    {
+    fun fromList(data: List<String>): String {
         return data.joinToString("|||")
     }
 }
--- app/src/main/java/ru/gmpopov/recipeapp/data/database/entity/RecipeEntity.kt
+++ app/src/main/java/ru/gmpopov/recipeapp/data/database/entity/RecipeEntity.kt
@@ -1,12 +1,14 @@
 package ru.gmpopov.recipeapp.data.database.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity("recipes")
+@Entity(tableName = "recipes")
 data class RecipeEntity(
     @PrimaryKey val id: Int,
     val title: String,
+    @ColumnInfo("category_id")
     val categoryId: Int,
     val imageUrl: String,
     val ingredients: String,
