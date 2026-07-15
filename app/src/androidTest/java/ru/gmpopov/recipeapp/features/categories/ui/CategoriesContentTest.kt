0c68d405e8cbbe26a1b2de1d3175fefda6f15e9a
gpopov <pgriga@mail.ru>
2026-07-15 19:46:45 +0500

    RCA-50: create test functions clickingCategoryNavigatesToRecipes and showsLoadingState

--- app/src/androidTest/java/ru/gmpopov/recipeapp/features/categories/ui/CategoriesContentTest.kt
+++ app/src/androidTest/java/ru/gmpopov/recipeapp/features/categories/ui/CategoriesContentTest.kt
@@ -3,7 +3,10 @@ package ru.gmpopov.recipeapp.features.categories.ui
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import junit.framework.TestCase.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import ru.gmpopov.recipeapp.features.categories.presentation.model.CategoriesUiState
@@ -34,6 +37,47 @@ class CategoriesContentTest {
 
         composeTestRule
             .onNodeWithText("БУРГЕРЫ")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun clickingCategoryNavigatesToRecipes() {
+        var clickedId: Int? = null
+        composeTestRule.setContent {
+            CategoriesContent(
+                CategoriesUiState(
+                    listOf(
+                        CategoryUiModel(
+                            id = 1,
+                            title = "Бургеры",
+                            description = "Бургеры",
+                            "burger.jpg"
+                        )
+                    )
+                ), onCategoryClick = { id, _, _ -> clickedId = id }, Modifier
+            )
+        }
+
+        composeTestRule
+            .onNodeWithText("БУРГЕРЫ")
+            .performClick()
+
+        assertEquals(1, clickedId)
+    }
+
+    @Test
+    fun showsLoadingState() {
+        composeTestRule.setContent {
+            CategoriesContent(
+                CategoriesUiState(
+                    isLoading = true,
+                ),
+                onCategoryClick = { _, _, _ -> }, Modifier,
+            )
+        }
+
+        composeTestRule
+            .onNodeWithTag("loading_indicator")
             .assertIsDisplayed()
     }
 }
